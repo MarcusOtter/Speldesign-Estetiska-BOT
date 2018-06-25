1d3d2ce8ee31ec3676c9f8f0051dacb9e961212c
Marcus Otterström <marcus.otterstrom@gmail.com>
2018-06-25 16:38:37 +0200

    Added IDataStorage implementation InMemoryStorage
    Added simple implementation InMemoryStorage.cs

---
+++ SpeldesignBotCore/Storage/Implementations/InMemoryStorage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SpeldesignBotCore.Storage.Implementations
+{
+    public class InMemoryStorage : IDataStorage
+    {
+        private readonly Dictionary<string, object> _dictionary = new Dictionary<string, object>();
+
+        public void StoreObject(object obj, string key)
+        {
+            if (_dictionary.ContainsKey(key)) return;
+            
+            _dictionary.Add(key, obj);
+        }
+
+        public T RestoreObject<T>(string key)
+        {
+            if (!_dictionary.ContainsKey(key))
+            {
+                throw new ArgumentException($"The provided key '{key}' could not be found.");
+            }
+
+            return (T) _dictionary[key];
+        }
+    }
+}
