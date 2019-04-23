eef7f286c2f2443ba5fd5a001ccf9d274a9e64d2
Marcus Otterström <marcus.otterstrom@gmail.com>
2019-04-23 22:31:06 +0200

    Add Unity unit tests

---
+++ SpeldesignBotCore.xUnit.Tests/UnityTests.cs
@@ -0,0 +1,19 @@
+﻿using SpeldesignBotCore.Storage;
+using Xunit;
+
+namespace SpeldesignBotCore.xUnit.Tests
+{
+    public class UnityTests
+    {
+        [Fact]
+        public void UnityResolveTwoObjectsTest()
+        {
+            var storage1 = Unity.Resolve<IDataStorage>();
+            var storage2 = Unity.Resolve<IDataStorage>();
+
+            Assert.NotNull(storage1);
+            Assert.NotNull(storage2);
+            Assert.Same(storage1, storage2);
+        }
+    }
+}
