cf1ed445f80a6208da32718b38fce73a779e3353
Marcus Otterström <marcus.otterstrom@gmail.com>
2019-05-29 18:50:21 +0200

    Add tests for json storage
    Closes #6

--- SpeldesignBotCore/Storage/Implementations/JsonStorage.cs
+++ SpeldesignBotCore/Storage/Implementations/JsonStorage.cs
@@ -1,6 +1,5 @@
 ﻿using System.IO;
 using Newtonsoft.Json;
-using static System.IO.Directory;
 using static Newtonsoft.Json.JsonConvert;
 
 namespace SpeldesignBotCore.Storage.Implementations
@@ -9,14 +8,31 @@ namespace SpeldesignBotCore.Storage.Implementations
     {
         public T RestoreObject<T>(string key)
         {
-            var json = File.ReadAllText($"{key}.json");
+            string json = string.Empty;
+
+            try
+            {
+                json = File.ReadAllText($"{key}.json");
+            }
+            catch (FileNotFoundException)
+            {
+                throw new System.ArgumentException($"The provided key '{key}' could not be found.");
+            }
+
             return DeserializeObject<T>(json);
         }
 
         public void StoreObject(object obj, string key)
         {
             var filePath = $"{key}.json";
-            CreateDirectory(Path.GetDirectoryName(filePath));
+            var directoryName = Path.GetDirectoryName(filePath);
+
+            if (directoryName == string.Empty)
+            {
+                throw new System.ArgumentException("Key must be at least 1 directory deep");
+            }
+
+            Directory.CreateDirectory(directoryName);
             var json = SerializeObject(obj, Formatting.Indented);
             File.WriteAllText(filePath, json);
         }
