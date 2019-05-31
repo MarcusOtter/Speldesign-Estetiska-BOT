77d4b77a50462a68e5af21075aed983fd2aaa762
Marcus Otterström <marcus.otterstrom@gmail.com>
2019-05-31 02:04:44 +0200

    Add custom attribute for commands
    Will help to showcase usage and describe the command.

---
+++ SpeldesignBotCore/Modules/Attributes/CommandInfoAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SpeldesignBotCore
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+    public class CommandInfoAttribute : Attribute
+    {
+        /// <summary>Explains how to use the command. Omit square brackets.</summary>
+        public readonly string Usage;
+
+        /// <summary>Explains what the command does.</summary>
+        public readonly string Description;
+
+        /// <param name="usage">Explains how to use the command. Omit square brackets.</param>
+        /// <param name="description">Explains what the command does.</param>
+        public CommandInfoAttribute(string usage, string description)
+        {
+            Usage = usage;
+            Description = description;
+        }
+    }
+}
